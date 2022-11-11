0a4c62e26a2c9f61b717058983c24e4ff56ca8ff
patteesolomon <theshiningkillerrashakublade@outlook.com>
2022-11-10 18:42:49 -0600

    Add files via upload
    k

--- 2PPsyBattle/src/index.js
+++ 2PPsyBattle/src/index.js
@@ -1,12 +1,23 @@
-// 25 pts for total cust
+//var canvas = document.querySelector('canvas');
+//console.log(canvas);
+//canvas.width = window.innerWidth;
+//canvas.height = window.innerHeight;
+
+// pts for total cust, 0 - 11 properties
 const ACR = 
 {
     hp: 6,
-    atk: 4,
-    acc: 3,
-    def: 3,
-    psy: 7,
-    sta: 2
+    atk: 2,
+    acc: 0.2,
+    def: 2,
+    psi: 11,
+    sta: 2,
+    eth: 5,
+    pos: 10,
+    neg: 0,
+    maxAtbCharge: 0,
+    currentATBCharge: 1,
+    charN: ''
 };
 
 var z = document.getElementById('z');
@@ -29,13 +40,40 @@ let psmin = 1;
 let psmax = 5;
 let stmin = 1;
 let stmax = 5;
+let posmin = 1;
+let posmax = 15;
+let negmin = -15; // reversed
+let negmax = 0; // plane
 
-randT = (hmax, hmin) =>
-    {
-        return Math.floor(Math.random() * (hmax - hmin + 1)) + hmin;
-    };
+function setPlATBC(PT)
+{
+    log(PT[0]); // this works
+}
+
+function playerATBCharge(PT)
+{
+    log(PT[]);
+}
 
 var logMe = document.getElementById('logBox');
+function outputColor(idn){
+    logMe.style.color = `${idn}`;
+}
+function log(l)
+{
+    logMe.innerText += '\n' + l;
+} 
+
+log('hello world');
+
+// normal output
+outputColor('white');
+// dmg 
+//outputColor('gold');
+
+// dialogue
+//outputColor('blue');
+//outputColor('darkblue'); // noice
 
 var coinTossed = false;
 var retreatA = false;
@@ -46,10 +84,31 @@ var winnerChosen = false;
 var pointP1C = 0;
 var pointP2C = 0;
 
+let P1 = ACR[8]; // cust these stats
+let P2 = ACR[8]; // cust these stats
 
-let P1 = ACR[5]; // cust these stats
-let P2 = ACR[5]; // cust these stats
 
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+
+randT = (hmax, hmin) =>
+{
+    return Math.floor(Math.random() * (hmax - hmin + 1)) + hmin;
+};
+
+// randomizer 6 stats are set and working
+P1 = [randT(hpmax, hpmin),randT(atmax, atmin),randT(accmax, accmin),randT(dfmax, dfmin),randT(psmax, psmin),randT(stmax, stmin), randT(10, 7), randT(0, 4), 0, 1, ''];
+
+P2 = [randT(hpmax, hpmin),randT(atmax, atmin),randT(accmax, accmin),randT(dfmax, dfmin),randT(psmax, psmin),randT(stmax, stmin), randT(10, 7), randT(0, 4), 0, 1, ''];
+
+function DecreasedATB(factor = 1)
+{
+    currentATBCharge = clamp(currentATBCharge - (factor * maxAtbCharge), 0, maxAtbCharge);
+}
+
+function setCurrentATB(newCharge)
+{
+    currentATBCharge = newCharge;
+}
 
 // controls
 showOptionsP1 = () =>
@@ -89,115 +148,142 @@ hideOptionsP2 = () =>
         battleProcessing();
     }
 
-    
+//setPlATBC(P1);
 
-// randomizer 6 stats are set and working
-P1 = [randT(hpmax, hpmin), randT(atmax, atmin), randT(accmax, accmin), randT(dfmax, dfmin), randT(psmax, psmin), randT(stmax, stmin)];
+    // await means waiting on the function in que then execution of everything else.
 
-P2 = [randT(hpmax, hpmin),randT(atmax, atmin),randT(accmax, accmin),randT(dfmax, dfmin),randT(psmax, psmin),randT(stmax, stmin)];
-
-    // await means waiting on the function in q then execution of everything else.
-
-    function endGame() {
+    function endGame() 
+    {
         console.log('End Theme. Cue..');
         logMe.innerText = 'You win this war';
-        console.log('You have to create a new future');
+        logMe.innerText = 'You have to create a new future.';
         hideOptions();
         return;
     }
-    // async battle functions
-    async function playerPhase(PT)
+
+    function actionBase(PT)
     {
-        return new Promise((PT, reject) => {
-            if (p1T == true) // player 1
-            {
-                console.log();
+        logMe.innerText += 'Attack / Psi / Defend';
+        z.onclick = function() {
+            logMe.innerText +='attacking!!!';
+            allydmgLog();
+        };
+        x.onclick = function() {
+            if (PT[6] <= 0) {
+                logMe.innerText += 'Insufficient Ether.';
+                //PT.miss = 0;
             }
-            else // player 2
-            {
-                console.log();
+            else{
+                //PT.miss -= 1;
+                logMe.innerText += 'Using Psi';
+                //allyM();
             }
-            console.log('');
-            //dmg calc
-        console.log('Your turn...');
-        showOptionsP1();
-        });
+        };
+        c.onclick = function() {
+            logMe.innerText+='defence';
+            retreat();
+        };
+    }
+
+    // async battle functions
+    async function playerPhase()
+    {  
+        if (p1T == true) // player 1
+        {
+            logMe.innerText += `\n`+'Player1 turn...';
+            showOptionsP1();
+        }
+        else if (p2T == true)// player 2
+        {
+            logMe.innerText += 'Player2 turn...';
+            showOptionsP2();
+        }
     }
 
     async function coinToss()
     {
         // heads 1 beats tails 0
         //p1 cointoss
-        if(randT(0,1) == 0)
+        if (randT(0, 100) == 0)
         {
             p1T = false;
         }
         
-        if (randT(0,1) == 1) 
+        if (randT(0, 100) == 1) 
         {
             p1T = true;
         }
         // redo 
         //p2 cointoss
-        if(randT(0,1) == 0)
+        if (randT(0, 100) == 0)
         {
             p2T = false;
         }
         
-        if (randT(0,1) == 1) 
+        if (randT(0, 100) == 1) 
         {
             p2T = true;
         }
         coinTossed = true;
+        log(`${p1T}`);
+    }
+    
+    async function timerThf(tr, atbRate)
+    {
+        timer = setInterval(()=>{
+        tr--;
+            if (tr <= 0) {
+                return;
+            }
+        }, 1000 * atbRate);
     }
 
-    async function timerThf(tr)
-    {
-         // start / end
-        tr.start(0);
-        tr.end(tr);
-    }
+    //hideOptionsP1();
+    //hideOptionsP2();
 
     async function battleProcessing()
+{
+    hideOptionsP1();
+    hideOptionsP2();
+    // player coin toss
+    if (coinTossed == false) 
     {
-        hideOptionsP1();
-        hideOptionsP2();
-        // player coin toss
-        if (coinTossed == false) 
+        await coinToss();
+        if (p1T == true && p2T == true) 
         {
-            await coinToss();
-            if (p1T == true && p2T == true) 
-            {
-                alert('retoss!');
-                coinTossed = false;
-                battleProcessing();
-            }
+            alert('retoss!');
+            coinTossed = false;
+            battleProcessing();
         }
-        else if(P1[0] <= 0 && P2[0] > 0)
-        {
-            // call winner 
-        }
-        else if(P2[0] <= 0 && P1[0] > 0)
-        {
-            //call winner
-        }
-        else if (p1T == true && p2T == false)
-        {
-            await playerPhase(P1);
-            await timerThf(300);
-            await playerPhase(P2);
-            //p1T = false; there's no reason to change this hierarchy
-        }
-        else if (p2T == true && p1T == false)
-        {
-            await playerPhase(P2);
-            await timerThf(300);
-            await playerPhase(P1);
-            //p2T = false; leave this here like this.
-            // leave this alone.
-        }
-        await timerThf(300); // thirty frames
     }
+    else if(P1[0] <= 0 && P2[0] > 0)
+    {
+        // call winner 
+    }
+    else if(P2[0] <= 0 && P1[0] > 0)
+    {
+        //call winner
+    }
+    else if (p1T == true && p2T == false)
+    {
+        playerPhase();
+        timerThf(3, 1);
+        playerPhase();
+        //p1T = false; there's no reason to change this hierarchy
+    }
+    else if (p2T == true && p1T == false)
+    {
+        playerPhase();
+        timerThf(3, 1);
+        playerPhase();
+        //p2T = false; leave this here like this.
+        // leave this alone.
+    }
+    timerThf(3); // thirty frames
+}
 
-    logMe.innerText = P1[0];
-    playerPhase(P1);
+
+log('uhi');
+
+
+battleProcessing();
